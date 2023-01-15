d0f0f71b2790228c6b3b731d59616801c1d4d1ae
jahnvi15 <91893464+jahnvi15@users.noreply.github.com>
2023-01-15 18:43:58 +0530

    d

--- user/src/pages/register/Register.jsx
+++ user/src/pages/register/Register.jsx
@@ -13,7 +13,7 @@ export default function Register() {
     e.preventDefault();
     setError(false);
     try {
-      const res = await axios.post("/auth/register", {
+      const res = await axios.post("http://localhost:8800/blog/auth/register", {
         username,
         email,
         password,
